e1a4fb8b266c20d834b8a1e33714daab3963df98
cophenetic <cophenetic@gmail.com>
2013-02-20 14:16:44 -0800

    master commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FourthApp::Application.config.secret_token = '9c2d1a6737aea4f676696123aae4439c5446142e34a4ede4d53e2fe3664f6cba24fa0d963070ca5411d3e88a022400646c9c084938267949a53cae7480d07dd0'
